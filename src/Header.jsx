dbb60d2af342a4ebf12860a7b35be289a9a00b81
kamruzzamanBappy <kamruzzamanbappy13@gmail.com>
2025-05-05 16:51:55 +0600

    d4

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,24 +7,24 @@ import ShoppingCart from './assets/shopping-cart.svg';
 const Header = () => {
   return (
     <header>
-    <nav class="container flex items-center justify-between space-x-10 py-6">
+    <nav className="container flex items-center justify-between space-x-10 py-6">
         <a href="index.html">
             <img src={ Logo} width="139" height="26" alt="" />
         </a>
 
-        <ul class="flex items-center space-x-5">
+        <ul className="flex items-center space-x-5">
             <li>
-                <a class="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
+                <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
                     <img src={Ring}  width="24" height="24" alt="" />
                 </a>
             </li>
             <li>
-                <a class="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
+                <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
                     <img src={Moon} width="24" height="24" alt="" />
                 </a>
             </li>
             <li>
-                <a class="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
+                <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
                     <img src={ShoppingCart}width="24" height="24" alt="" />
                 </a>
             </li>
--- src/data/movies.js
+++ src/data/movies.js
@@ -1,11 +1,55 @@
 const data = [
     {
         "id": crypto.randomUUID() ,
-        "cover":" ",
-        "title": "",
-        "description":"",
-        "genre":"",
+        "cover":"once-in-ho.jpg",
+        "title": "Once Upon a Time... in Hollywood",
+        "description":"A fadded televession actor and stunt double strive to acheive fame",
+        "genre":"Commedy/Drama",
+        "rating":5,
+        "price": 140
+    },
+    {
+        "id": crypto.randomUUID() ,
+        "cover":"marriage-strory.jpg",
+        "title": "Marriage Story",
+        "description":"A fadded televession actor and stunt double strive to acheive fame",
+        "genre":"Commedy/Drama",
+        "rating":3,
+        "price": 140
+    },
+    {
+        "id": crypto.randomUUID() ,
+        "cover":"pain-and-gain.jpg",
+        "title": "Pain & Gain",
+        "description":"A fadded televession actor and stunt double strive to acheive fame",
+        "genre":"Action/Commedy/Drama",
+      
+        "rating":4,
+        "price": 140
+    },
+    {
+        "id": crypto.randomUUID() ,
+        "cover":"avatar.png",
+        "title": "Avatar",
+     "description":"A fadded televession actor and stunt double strive to acheive fame",
+        "genre":"Action/Commedy/Drama",
+        "rating":5,
+        "price": 140
+    },
+    {
+        "id": crypto.randomUUID() ,
+        "cover":"parasite.jpg",
+        "title": "Love",
+      "description":"A fadded televession actor and stunt double strive to acheive fame",
+        "genre":"Action/Commedy/Drama",
         "rating":5,
         "price": 140
     }
 ]
+
+function getAllMovies(){
+    return data;
+}
+
+export { getAllMovies };
+
